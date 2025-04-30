96bdc1d67a96421d045519a7532f6adae8736383
onesb23 <ArtemS@1stopbedrooms.com>
2025-04-30 08:08:13 -0400

    Add datadog reciever

--- run-lgtm.sh
+++ run-lgtm.sh
@@ -6,11 +6,14 @@ docker pull docker.io/grafana/otel-lgtm:"${RELEASE}"
 
 touch .env
 
+mkdir -vp container/grafana container/prometheus container/loki
+
 docker run \
 	--name lgtm \
-	-p 3000:3000 \
+	-p 3001:3000 \
 	-p 4317:4317 \
 	-p 4318:4318 \
+        -p 8126:8126 \
 	--rm \
 	-ti \
 	-v "$PWD"/container/grafana:/data/grafana \
@@ -18,4 +21,4 @@ docker run \
 	-v "$PWD"/container/loki:/data/loki \
 	-e GF_PATHS_DATA=/data/grafana \
 	--env-file .env \
-	docker.io/grafana/otel-lgtm:"${RELEASE}"
+	docker.io/onesb/otel-lgtm:"${RELEASE}"
